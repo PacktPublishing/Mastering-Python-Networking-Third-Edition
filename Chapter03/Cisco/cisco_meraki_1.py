927c9550f86089a1662878eacbce012b02d2aea2
Eric Chou <echou@yahoo.com>
2019-10-19 17:17:58 -0700

    modified meraki examples

--- Chapter03/Cisco/cisco_meraki_1.py
+++ Chapter03/Cisco/cisco_meraki_1.py
@@ -2,7 +2,7 @@
 import requests
 import pprint
 
-myheaders={'X-Cisco-Meraki-API-Key': '6bec40cf957de430a6f1f2baa056b99a4fac9ea0'}
+myheaders={'X-Cisco-Meraki-API-Key': ''}
 url = 'https://dashboard.meraki.com/api/v0/organizations'
 response = requests.get(url, headers=myheaders, verify=False)
 pprint.pprint(response.json())
--- Chapter03/Cisco/cisco_meraki_2.py
+++ Chapter03/Cisco/cisco_meraki_2.py
@@ -2,8 +2,8 @@
 import requests
 import pprint
 
-myheaders={'X-Cisco-Meraki-API-Key': '6bec40cf957de430a6f1f2baa056b99a4fac9ea0'}
-orgId = '549236'
+myheaders={'X-Cisco-Meraki-API-Key': ''}
+orgId = ''
 url = 'https://dashboard.meraki.com/api/v0/organizations/' + orgId + '/networks'
 response = requests.get(url, headers=myheaders, verify=False)
 pprint.pprint(response.json())
