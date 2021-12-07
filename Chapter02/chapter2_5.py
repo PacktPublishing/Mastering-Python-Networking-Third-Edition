0c1a6ea557710f3b12cdb310dd9cb4ecca2dbce3
Eric Chou <echou@yahoo.com>
2021-12-07 15:50:04 -0800

    added change notes for Chapter02/chapter2_5.py example

--- Chapter02/chapter2_5.py
+++ Chapter02/chapter2_5.py
@@ -4,6 +4,13 @@ from nornir import InitNornir
 from nornir.plugins.tasks.networking import netmiko_send_command
 from nornir.plugins.functions.text import print_result
 
+# If you are using Nornir3, there are some changes:
+# https://github.com/twin-bridges/nornir_course/blob/master/nornir3_changes.md
+# Please pip install: pip install nornir_utils nornir_netmiko
+# Then replace the 2 nornir.plugin imports with below:
+#from nornir_utils.plugins.functions import print_result
+#from nornir_netmiko import netmiko_send_command
+
 nr = InitNornir()
 
 result = nr.run(
